d0e3ed2ee890bb5e46a0ad630a6f390d48af1552
svs <vsshmalko@gmail.com>
2022-05-11 13:33:51 +0300

    Add directors

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -1,12 +1,17 @@
 const graphql = require('graphql');
 
-const {GraphQLObjectType, GraphQLString, GraphQLSchema, GraphQLID} = graphql;
+const {GraphQLObjectType, GraphQLString, GraphQLSchema, GraphQLID, GraphQLInt} = graphql;
 
 const movies = [
-    {id: '1', name: 'PF', genre: 'Crime'},
-    {id: '2', name: 'Inception', genre: 'Fantastic'},
-    {id: 3, name: 'Interstellar', genre: 'SciFi'},
-    {id: 4, name: 'Tenet', genre: 'Fantastxic'}
+    {id: '1', name: 'PF', genre: 'Crime', directorId: '1'},
+    {id: '2', name: 'Inception', genre: 'Fantastic', directorId: '2'},
+    {id: 3, name: 'Interstellar', genre: 'Sci-Fi', directorId: '2'},
+    {id: 4, name: 'Tenet', genre: 'Fantastxic', directorId: '2'}
+];
+
+const directors = [
+    {id: '1', name: 'Quentin Tarantino', age: 122},
+    {id: '2', name: 'Christopher Nolan', age: 22}
 ];
 
 const MovieType = new GraphQLObjectType({
@@ -15,6 +20,19 @@ const MovieType = new GraphQLObjectType({
         id: {type: GraphQLID},
         name: {type: GraphQLString},
         genre: {type: GraphQLString},
+        director: {type: DirectorType,
+        resolve(parent,args){
+            return directors.find(director => director.id == parent.id);
+        }}
+    }),
+})
+
+const DirectorType = new GraphQLObjectType({
+    name: 'Director',
+    fields: () => ({
+        id: {type: GraphQLID},
+        name: {type: GraphQLString},
+        age: {type: GraphQLInt},
     }),
 })
 
@@ -26,6 +44,13 @@ const Query = new GraphQLObjectType({
             args: {id: {type: GraphQLID}},
             resolve(parent, args) {
                 return movies.find(movie => movie.id == args.id);
+            }
+        },
+        director: {
+            type: DirectorType,
+            args: {id: {type: GraphQLID}},
+            resolve(parent, args) {
+                return directors.find(director => director.id == args.id);
             }
         },
     }
